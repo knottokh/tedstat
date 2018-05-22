621e5deec095d1a41d10c092e84ed04e22ab7b8c
Knotto <one2treego@hotmail.com>
2018-05-22 00:14:41 +0000

    mo migration

--- db/migrate/20180521233341_add_columns_to_schools2.rb
+++ db/migrate/20180521233341_add_columns_to_schools2.rb
@@ -1,13 +1,5 @@
 class AddColumnsToSchools2 < ActiveRecord::Migration[5.1]
   def change
-    remove_column :scourses, :education_area
-    remove_column :scourses, :ministry_code
-    remove_column :scourses, :municipal_area
-    remove_column :scourses, :district
-    remove_column :scourses, :province
-    remove_column :scourses, :postcode
-    remove_column :scourses, :zone
-    remove_column :scourses, :students_count
     add_column :schools, :education_area,      :string 
     add_column :schools, :ministry_code,       :string 
     add_column :schools, :municipal_area,      :string 
