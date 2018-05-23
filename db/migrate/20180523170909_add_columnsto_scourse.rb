81b4ce18d10c92590f8fc7b01d829e294f2e755a
Knotto <one2treego@hotmail.com>
2018-05-23 18:05:18 +0000

    fix and update stuent number

--- db/migrate/20180523170909_add_columnsto_scourse.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnstoScourse < ActiveRecord::Migration[5.1]
-  def change
-    add_column :scourses, :order,      :integer 
-  end
-end
--- db/migrate/20180523172725_add_columnsto_scourse2.rb
+++ db/migrate/20180523172725_add_columnsto_scourse2.rb
@@ -1,6 +1,5 @@
 class AddColumnstoScourse2 < ActiveRecord::Migration[5.1]
   def change
-    remove_column :scourses, :order
     add_column :scourses, :studenno,      :integer 
   end
 end
